4676453c09bf771e77e6f0be4da0ed3ee4b6eed9
James <james.o.steph@gmail.com>
2021-04-15 11:08:40 -0600

    Added figure and body drawing

--- src/main/graph.py
+++ src/main/graph.py
@@ -1,5 +1,23 @@
-import plotly
+from plotly import graph_objects as go
+import numpy as np
 
 class Grapher:
-    def __init__():
+    def __init__(self):
         pass
+
+    def spheres(self, size, clr, centre_x, centre_y, centre_z): 
+    
+        # Set up 100 points. First, do angles
+        theta = np.linspace(0,2*np.pi,100)
+        phi = np.linspace(0,np.pi,100)
+        
+        # Set up coordinates for points on the sphere
+        x0 = size * np.outer(np.cos(theta),np.sin(phi)) + centre_x
+        y0 = size * np.outer(np.sin(theta),np.sin(phi)) + centre_y
+        z0 = size * np.outer(np.ones(np.size(theta)),np.cos(phi)) + centre_z
+        
+        # Set up trace
+        trace = go.Surface(x=x0, y=y0, z=z0, colorscale=[[0,clr], [1,clr]])
+        trace.update(showscale=False)
+
+        return trace
--- src/main/main.py
+++ src/main/main.py
@@ -1,9 +1,39 @@
 from oauth2client.service_account import ServiceAccountCredentials
-
+from plotly import graph_objects as go
+from graph import Grapher
 import gspread
 
 
 def main():
+    gr = Grapher()
+
+    layout=go.Layout(title = 'Solar System', showlegend=False,
+                #   paper_bgcolor = 'black',
+                  scene = dict(xaxis=dict(title='X', 
+                                          titlefont_color='black', 
+                                          range=[-10000000,10000000], 
+                                          backgroundcolor='black',
+                                          color='black',
+                                          gridcolor='black'),
+                               yaxis=dict(title='Y',
+                                          titlefont_color='black',
+                                          range=[-10000000,10000000],
+                                          backgroundcolor='black',
+                                          color='black',
+                                          gridcolor='black'
+                                          ),
+                               zaxis=dict(title='Z', 
+                                          titlefont_color='black',
+                                          range=[-10000000,10000000],
+                                          backgroundcolor='black',
+                                          color='white', 
+                                          gridcolor='black'
+                                         ),
+                               aspectmode='manual', #this string can be 'data', 'cube', 'auto', 'manual'
+           #a custom aspectratio is defined as follows:
+                               aspectratio=dict(x=1, y=1, z=1),
+                               annotations=[]
+                               ))
     ########################## Setup google credentials and API access ##########################################
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 
@@ -20,3 +50,13 @@ def main():
     for record in records_dict:
         # Make solar bodies
         print(record)
+        objects.append(gr.spheres(50000,'#ffff00',record['X'], record['Y'], record['Z']))
+    
+    
+    
+    fig = go.Figure(layout = layout)
+    for item in objects:
+        fig.add_trace(item)
+
+    fig.show()
+    fig.write_html("Solar_system.html")
