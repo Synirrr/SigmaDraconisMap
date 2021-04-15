1cad633cd74fe4f44414058a0367c59f87875534
James <james.o.steph@gmail.com>
2021-04-14 21:20:58 -0600

    Added graph.py and authentication for sheet

---
+++ src/main/graph.py
@@ -0,0 +1,5 @@
+import plotly
+
+class Grapher:
+    def __init__():
+        pass
--- src/main/main.py
+++ src/main/main.py
@@ -1,3 +1,22 @@
+from oauth2client.service_account import ServiceAccountCredentials
+
+import gspread
+
 
 def main():
-    print("Hello World!")
+    ########################## Setup google credentials and API access ##########################################
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('src/main/starmap-310623-baf93c6b9991.json', scope)
+
+    gc = gspread.authorize(credentials)
+    #############################################################################################################
+
+    # Get records from spread sheet
+    wks = gc.open('CSV-SDW-GPS').sheet1
+
+    records_dict = wks.get_all_records()
+    objects = []
+    for record in records_dict:
+        # Make solar bodies
+        print(record)
